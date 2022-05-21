c03862a7bc5df473e8bbf378cca95b546f914a81
Linda Damayanti <linda.damayanti.tif419@polban.ac.id>
2022-05-21 22:22:20 +0700

    Footer Done
    Alhamdulillah footer beres

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,17 +1,118 @@
 <template>
   <v-app id="footer">
-    <v-content>
-        <v-footer
-        padless>
-            <v-card width="1920px" height="250px" color="#003294">
-            </v-card>
-            <v-card width="1920px" color="#10203F">
-            <v-card-text class="text-center">
-                Jurusan Teknik Komputer dan Informatika Politeknik Negeri Bandung
-            </v-card-text>
-            </v-card>
-        </v-footer>
-    </v-content>
+    <div>
+      <v-footer color="#003399">
+  
+
+          <v-row >
+          <v-col
+            cols="12"
+            sm="3"
+          >
+
+          <v-list-item three-line>
+            <v-list-item-avatar
+
+            ></v-list-item-avatar>
+
+            <v-list-item-content>
+              <h4 class="white--text mb-lg-4">
+                Akreditasi Program Studi 
+              </h4>
+              <v-img 
+              class="mb-lg-4"  
+              max-height="150"
+              max-width="245"
+              src="../assets/D3.png"></v-img>
+              <v-img   
+              max-height="150"
+              max-width="245"
+              src="../assets/D4.png"></v-img>
+            </v-list-item-content>
+          </v-list-item>
+     
+          </v-col>
+
+          <v-col
+            cols="12"
+            sm="3"
+          >
+
+          <v-list-item three-line>
+            <v-list-item-avatar
+
+            ></v-list-item-avatar>
+
+            <v-list-item-content>
+              <h4 class="white--text mb-lg-4">
+                Kontak
+              </h4>
+              <h6 class="white--text">email </h6>
+              <h6 class="white--text mb-lg-4">contact@jtk.polban.ac.id</h6>
+              
+                 <h6 class="white--text">Hotline JTK </h6>
+                 <h6 class="white--text">022123456 </h6>
+            </v-list-item-content>
+          </v-list-item>
+     
+          </v-col>
+
+                    <v-col
+            cols="12"
+            sm="3"
+          >
+
+          <v-list-item three-line>
+            <v-list-item-avatar
+
+            ></v-list-item-avatar>
+
+            <v-list-item-content>
+              <h4 class="white--text mb-lg-4">
+                Link Eksternal
+              </h4>
+               <h6 class="white--text">polban.ac.id </h6>
+              <h6 class="white--text">akademik@polban.ac.id</h6>
+              <v-list-item-subtitle></v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+     
+          </v-col>
+
+                    <v-col
+            cols="12"
+            sm="3"
+          >
+
+          <v-list-item three-line>
+            <v-list-item-avatar
+
+            ></v-list-item-avatar>
+
+            <v-list-item-content>
+              <h4 class="white--text mb-lg-4">
+                Media Sosial
+              </h4>
+             
+             <v-row class="ml-lg-1 pt-lg-1"> <v-icon color="white" large  class="mr-lg-1">mdi-facebook</v-icon>
+              <v-icon color="white" large>mdi-youtube</v-icon></v-row>
+              <v-row class="ml-lg-1"><v-icon color="white" large class="mr-lg-1">mdi-instagram</v-icon>
+              <v-icon color="white" large>mdi-twitter</v-icon></v-row>
+          
+              
+            </v-list-item-content>
+          </v-list-item>
+     
+          </v-col>
+      </v-row>
+    
+  </v-footer>
+      <v-card tile width="2000" color="#10203F">
+      <v-card-text class="text-center" >
+          <h5 style="color:#ffffff">Jurusan Teknik Komputer dan Informatika Politeknik Negeri Bandung</h5>
+        </v-card-text>
+      </v-card>
+    </div>
   </v-app>
 </template>
 
