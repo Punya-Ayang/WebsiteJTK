77a454c349b63a52134f381fc922f188820f099d
Linda Damayanti <linda.damayanti.tif419@polban.ac.id>
2022-05-29 07:22:09 +0700

    Delete meili_data

--- cms-web-jtk/config/plugins.js
+++ cms-web-jtk/config/plugins.js
@@ -1,11 +1,4 @@
 module.exports = ({ env }) => ({
-  meilisearch: {
-    config: {
-      // Your meili host
-      host: "http://localhost:7700",
-      // Your master key or private key
-      apiKey: "masterKey",
-    }},
     upload: {
       config: {
         provider: "cloudinary",
