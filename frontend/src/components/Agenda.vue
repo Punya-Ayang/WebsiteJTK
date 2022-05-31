2f3f8cb17efee0b076fbc4cf79f5965bf7bd85f0
marissanura <54462895+marissanura@users.noreply.github.com>
2022-05-31 20:58:23 +0700

    :hammer: Date Picker and Agenda Integration

--- frontend/src/components/Agenda.vue
+++ frontend/src/components/Agenda.vue
@@ -15,24 +15,23 @@
         <v-container style="display: flex">
           <v-container>
             <v-col v-for="(item, i) in items" :key="i" cols="12">
-              <v-card
+              <v-card v-if="new Date(item.tanggal_kegiatan) >= new Date(dates[0]) && new Date(item.tanggal_kegiatan) <= new Date(dates[1])"
                 style="border: 1px solid white"
                 max-width="360"
                 outlined
                 color="ffffff"
               >
                 <v-list-item three-line>
-                  <v-list-item-avatar
-                    tile
-                    size="100"
-                  ><v-img :src="item.src"></v-img></v-list-item-avatar>
-                  
+                  <v-list-item-avatar tile size="100"
+                    ><v-img :src="item.image.url"></v-img
+                  ></v-list-item-avatar>
 
                   <v-list-item-content>
-                    <v-list-item-title class="text-h6 mb-1" v-text=item.title>
+                    <v-list-item-title class="text-h6 mb-1" v-text="item.judul_kegiatan">
                     </v-list-item-title>
-                    <v-list-item-subtitle v-text=item.date></v-list-item-subtitle
-                    >
+                    <v-list-item-subtitle
+                      v-text="item.tanggal_kegiatan"
+                    ></v-list-item-subtitle>
                   </v-list-item-content>
                 </v-list-item>
               </v-card>
@@ -42,6 +41,7 @@
             <v-container class="ml-lg-16 pl-lg-16">
               <v-text-field
                 v-model="dateRangeText"
+                :available-dates='{ start: new Date(), end: null }'
                 label="Date range"
                 prepend-icon="mdi-calendar"
                 readonly
@@ -56,33 +56,37 @@
   </v-app>
 </template>
 <script>
+import axios from "axios";
+
 export default {
   el: "#app",
+  created() {
+    this.today = new Date();
+    this.today = String(this.today.getFullYear()) + '-' + String(this.today.getMonth() + 1).padStart(2, '0') + '-'+ String(this.today.getDate()).padStart(2, '0')
+    var date = this.today;
+    this.dates = [date, date];
+    console.log(date);
+    axios
+      .get("http://localhost:1337/acaras?tanggal_kegiatan_gt=" + date)
+      .then((res) => {
+        this.items = res.data;
+        console.log(this.items);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
   data: () => ({
-    dates: ["2019-09-10", "2019-09-20"],
-    items: [
-    {
-      src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
-      title: "Baksos Resiliensi",
-      date: "2019-09-10"
-    },
-    {
-      src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
-      title: "JTK Berbagi",
-      date: "2019-09-12"
-    },
-    {
-      src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
-      title: "Bukber JTK",
-      date: "2019-09-14"
-    }
-  ],
+    today: [],
+    dates: [],
+    items: [],
   }),
   computed: {
     dateRangeText() {
       return this.dates.join(" ~ ");
     },
   },
+
   dialog: false,
 };
 </script>
