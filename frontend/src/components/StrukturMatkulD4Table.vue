cdf7add4eefb497835712c525cac3301bc8290ad
marissanura <54462895+marissanura@users.noreply.github.com>
2022-05-31 18:47:45 +0700

    :hammer: Integrate Tabel Mata Kuliah

--- frontend/src/components/StrukturMatkulD4Table.vue
+++ frontend/src/components/StrukturMatkulD4Table.vue
@@ -1,85 +1,86 @@
 <template>
-    <div id="struktur-matkul-d4">
-        <v-content>
-            <div class="headline-middle">
-                <h1>Struktur Mata Kuliah</h1>
-            </div>
-            <v-container>
-                       <v-col
-                        class="mx-1 pl-0 pb-0"
-                        style="max-width: 10vw; min-width:1vw;"
-                        >
-                          <v-select
-                            :items="semester"
-                            label="Pilih Semester"
-                            dense
-                            solo
-                            color="#fffff"
-                          ></v-select>
-                        </v-col>
-            
-                <div class="subheader-mtk pb-lg-5 pt-0">
-                    <h1>D4 Teknik Informatika</h1>
-                </div>
-                <div>
-                    <v-data-table
-                        dense
-                        :headers="headers"
-                        :items="matkul"
-                        item-key="name"
-                        class="elevation-1"
-                    ></v-data-table>
-                </div>
-            </v-container>
-        </v-content>
-    </div>
+  <div id="struktur-matkul-d4">
+    <v-content>
+      <div class="headline-middle">
+        <h1>Struktur Mata Kuliah</h1>
+      </div>
+      <v-container>
+        <v-col class="mx-1 pl-0 pb-0" style="max-width: 10vw; min-width: 1vw">
+          <v-select
+            :items="semester"
+            label="Pilih Semester"
+            v-model="defaultSelected"
+            @input="atInput($event)"
+            dense
+            solo
+            color="#fffff"
+          ></v-select>
+        </v-col>
+
+        <div class="subheader-mtk pb-lg-5 pt-0">
+          <h1>D4 Teknik Informatika</h1>
+        </div>
+        <div>
+          <v-data-table
+            dense
+            :headers="headers"
+            :items="matakuliah[currSem - 1].mata_kuliah"
+            item-key="name"
+            class="elevation-1"
+          ></v-data-table>
+        </div>
+      </v-container>
+    </v-content>
+  </div>
 </template>
 
 <style>
-
 </style>
 
 <script>
+import axios from "axios";
+
 export default {
-    data: () => ({
-      matkul: [
-        {
-          name: 'Matematika Terapan',
-          deskripsi: 'Mata kuliah dasar yang mempelajari tentang kalkulus I',
-          sks: 3,
-          skst: 3,
-          sksp: 0,
-          kode: '16KU34052',
-          smt:'Semester 1'
-        },
-        {
-          name: 'Dasar Dasar Pemrograman',
-          deskripsi: 'Mata kuliah dasar untuk mempelajari algoritma',
-          sks: 4,
-          skst: 2,
-          sksp: 2,
-          kode: '16TIN4024',
-          smt:'Semester 1'
+  created() {
+    axios
+      .get("http://localhost:1337/struktur-mata-kuliahs?prodi=D4&_sort=semester:ASC")
+      .then((res) => {
+        this.matakuliah = res.data;
+        for (let i = 0; i < this.matakuliah.length; i++) {
+          this.semester.push(i + 1);
+          for (let j = 0; j < this.matakuliah[i].mata_kuliah.length; j++) {
+            this.matakuliah[i].mata_kuliah[j].sks = parseInt(this.matakuliah[i].mata_kuliah[j].sks_t + this.matakuliah[i].mata_kuliah[j].sks_p); 
+          }
         }
-      ],
-      headers: [
-        {
-          text: 'Nama Mata Kuliah',
-          align: 'start',
-          sortable: false,
-          value: 'name',
-        },
-        { text: 'Deskripsi', value: 'deskripsi',sortable: false },
-        { text: 'SKS', value: 'sks' },
-        { text: 'SKS Teori', value: 'skst' },
-        { text: 'SKS Praktek', value: 'sksp' },
-        { text: 'Kode', value: 'kode', sortable: false},
-        { text: 'Semester',value:'smt'}
-      ],
-      
-      semester :[
-        '1','2','3','4','5','6'
-      ],
-      }),
-}
+        console.log(this.matakuliah);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
+  methods: {
+    atInput(event) {
+      this.currSem =event;
+    }
+  },
+  data: () => ({
+    currSem: 1,
+    matakuliah: [],
+    headers: [
+      {
+        text: "Nama Mata Kuliah",
+        align: "start",
+        sortable: false,
+        value: "nama_mata_kuliah",
+      },
+      { text: "Deskripsi", value: "deskripsi", sortable: false },
+      { text: "SKS", value: "sks" },
+      { text: "SKS Teori", value: "sks_t" },
+      { text: "SKS Praktek", value: "sks_p" },
+      { text: "Kode", value: "kode", sortable: false },
+    ],
+    defaultSelected: 1,
+    semester: [],
+  }),
+};
 </script>
